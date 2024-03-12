fcea84e908d1912b9aaac82006e6f93d3d241dde
vidhusha18 <115626945+vidhusha18@users.noreply.github.com>
2024-03-12 09:46:15 +0530

    Updated sample.java in branch 1

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Myclass{
   public static void main(String args[]){
-    System.out.println("helloo from  github ");
+    System.out.println("helloo from  github branch 1  ");
   }
 }
