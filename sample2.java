faf6d69c4d7d799e1f2e75da786658ac3ee57dde
vidhusha18 <115626945+vidhusha18@users.noreply.github.com>
2024-03-12 10:00:12 +0530

    Updated  sample2.java by br2

--- sample2.java
+++ sample2.java
@@ -3,6 +3,7 @@ class Myclass2{
   public static void main(String args[]){
   System.out.println("hello world");
      System.out.println("hello earth");
+     System.out.println("hello venus");
     
   }
 }
